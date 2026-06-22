7f8fd588d7ac6f747d67aad68e9f5dd2a66ab5ad
suse <>
2026-06-23 08:38:58 +1000

    starting small layouting and testing

---
+++ app/join-queue/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>Enter Queue</h1>
+      <form>
+        <input type="text" placeholder="staff/student id" />
+        <button type="submit">Enter</button>
+      </form>
+    </main>
+  );
+}
